dd46af508623470f4b0b9085ad35337c6b0c8c6b
Hamjath <m.hamjath11@gmail.com>
2026-07-10 15:04:06 +0530

    Fix missing OnboardingTour import

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import ThemeToggle from '../components/ThemeToggle'
+import OnboardingTour from '../components/OnboardingTour'
 import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
